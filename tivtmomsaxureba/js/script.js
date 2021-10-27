a878297ad8dede9fba920386b341a65d3cac70b5
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2021-10-27 13:44:30 +0400

    js
    update

--- tivtmomsaxureba/js/script.js
+++ tivtmomsaxureba/js/script.js
@@ -1,22 +1,22 @@
 const products=document.getElementById("product-wrapper");
 let db=[
-    ["ჰეად ენდ შოლდერსი","12.4","./images/pc1.png"],
-    ["ნივეა","112.4"],
-    ["აჰავა","122.4"],
-    ["მესაუდა","42.4"],
-    ["ჰეად ენდ შოლდერსი","12.4"],
-    ["ნივეა","112.4"],
-    ["აჰავა","122.4"],
-    ["მესაუდა","42.4"],
-    ["ჰეად ენდ შოლდერსი","12.4"],
-    ["ნივეა","112.4"],
-    ["აჰავა","122.4"],
-    ["მესაუდა","42.4"],
+    ["ჰეად ენდ შოლდერსი","12.4","./images/pc1.png",2555],
+    ["ნივეა","112.4","./images/pc1.png",2],
+    ["აჰავა","122.4","./images/pc1.png",27],
+    ["მესაუდა","42.4","./images/pc1.png",28],
+    ["ჰეად ენდ შოლდერსი","12.4","./images/pc1.png",29],
+    ["ნივეა","112.4","./images/pc1.png",30],
+    ["აჰავა","122.4","./images/pc1.png",31],
+    ["მესაუდა","42.4","./images/pc1.png",32],
+    ["ჰეად ენდ შოლდერსი","12.4","./images/pc1.png",33],
+    ["ნივეა","112.4","./images/pc1.png",34],
+    ["აჰავა","122.4","./images/pc1.png",35],
+    ["მესაუდა","42.4","./images/pc1.png",36],
 
 ]
-let card= document.createElement("div")
+let card;
 function addCard(name,price,sour) {
-    let card= document.createElement("div");
+     card= document.createElement("div");
     let cardHeader=document.createElement("div");
     let cardImg=document.createElement("div");
     let cardName=document.createElement("div");
@@ -26,6 +26,7 @@ function addCard(name,price,sour) {
     cardName.setAttribute("class","card-name");
     cardPrice.setAttribute("class","card-price");
     card.setAttribute("class","product-card")
+    
     cardName.innerText=name;
     cardPrice.innerText=price;
     let img = document.createElement("img");
@@ -43,5 +44,30 @@ function addCard(name,price,sour) {
 }
 for (let i=0; i<db.length; i++){
     addCard(db[i][0],db[i][1],db[i][2]);
+    card.setAttribute("id",db[i][3])
 }
 
+
+
+let ids = [];
+let selectetCard;
+$(".product-card").on("click", function(){
+  // If the id is not already in the array, add it. If it is, remove it
+  if (ids.indexOf(this.id) === -1 ) {
+      ids.push(this.id);
+       selectetCard=document.getElementById(this.id);  
+      selectetCard.setAttribute("class","cardActive")
+    selectetCard.firstChild.style.backgroundImage ="url('./images/Checkbox_Checked@3x.png')"
+    } else { 
+        ids.splice(ids.indexOf(this.id),1); 
+        selectetCard=document.getElementById(this.id);  
+        selectetCard.setAttribute("class","product-card")
+        selectetCard.firstChild.style.backgroundImage ="none";
+
+    }
+      
+  
+  
+  
+  console.log(ids)
+});
